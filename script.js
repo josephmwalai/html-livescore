7145d2adb132ae6d09a4727407fd72c5051e5953
joseph mwalai <josephmwalai@gmail.com>
2024-09-25 11:20:47 +0300

    new update

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ let minutes = 87;
     timeLapseElement.innerHTML = `${minutes}<span clas="blinking-apostrophe>'</span>">`
  }
  setInterval(updateTimeLapse, 60000);
+
+ const buttons = document.querySelectorAll('.btn-icon');
+
+ buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        const icon = this.querySelector('i');
+        icon.classList.toggle('active');
+    });
+ });
